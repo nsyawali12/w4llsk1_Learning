6c4db8a7d47ad3b244293d17b36b305d01be0a0b
nsyawali12 <nsyawali12@gmail.com>
2019-02-25 04:04:24 +0700

    starting code

---
+++ Naive-Bayes Learn n app/nsyawalia-naive-bayes v0.py
@@ -0,0 +1,25 @@
+# w4llsk1_naive-bayes-first-project boissss
+test = ("Hello world!")
+project = ("This is my first project of Machine Learning")
+what = ("Project : Naive-Bayes")
+
+print (test)
+print (project)
+print (what)
+
+# import file using pandas and numpy for read the csv
+
+import pandas as pd
+import numpy as np
+
+dframe1 = pd.DataFrame(pd.read_csv('TrainsetTugas1ML.csv').as_matrix().tolist())
+dtrain = np.asarray(dframe1)
+stat = []
+
+for c in range(1,9):
+	label.append(list(set(dframe1[c].tolist())))
+
+# the reason why range 1-9 because the data train got 9 column
+
+# now creating the table model first before testing phase
+
